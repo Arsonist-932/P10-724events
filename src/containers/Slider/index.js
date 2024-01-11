cd096a5b4ea6cdc1d3315f5fa46706df22f1c899
Arsonist Tefh <119712506+Arsonist-932@users.noreply.github.com>
2024-01-11 02:26:53 +0100

    Fix Bug Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,13 +8,15 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    // Inversion en 1 -1 pour l'affichage décroissant
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
   );
+
+  // Création d'une variable pour récupérer la longueur du tableur focus
+  const ImgSlide = data?.focus.length;
+
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setTimeout(() => setIndex(index < ImgSlide - 1 ? index + 1 : 0), 5000);
   };
   useEffect(() => {
     nextCard();
@@ -22,36 +24,38 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {/* Ajout de focus pour créer les btn en fonction de focus dans le JSON */}
+          {byDateDesc?.map((focus, radioIdx) => (
+            <input
+              key={`${focus.title}`}
+              type="radio"
+              name="radio-button"
+              // Changement idx en index & ReadOnly pour gérer l'etat
+              checked={index === radioIdx}
+              readOnly
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
